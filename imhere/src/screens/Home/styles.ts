245882026eeb59ae6bd3aa9d9189fc710a761935
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-09-17 02:17:58 -0300

    start

---
+++ imhere/src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#131016',
+    flex: 1,
+    justifyContent: "center",
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontWeight: "bold",
+    fontSize: 24
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 16
+  }
+})
