efabf33609657806dbe9f548ac0f01028ad4f755
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-10-01 18:35:18 -0300

    module 2

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,11 +1,17 @@
+import  { useState } from 'react'
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 
 import { Container } from './styles';
+import { FlatList } from 'react-native';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container >
       <Header />
@@ -14,7 +20,21 @@ export function Groups() {
         subTitle='Jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do ignite'  />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1}}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma ?' />}
+      />
+
+        <Button 
+          title='Criar nova turma'
+        />
     </Container>
   );
 }
