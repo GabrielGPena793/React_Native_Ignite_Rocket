c6000fe6672148fa70323b9ad84bc77bb7e260bb
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2023-01-09 22:02:00 -0300

    creat custom error

--- igniteteams/src/storage/group/groupCreate.ts
+++ igniteteams/src/storage/group/groupCreate.ts
@@ -1,10 +1,18 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(newGroup: string) {
   try {
-    const storedGroups = await groupsGetAll()
+    const storedGroups = await groupsGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if(groupAlreadyExists) {
+      throw new AppError("Group already exists with name '" + newGroup)
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup])
 
