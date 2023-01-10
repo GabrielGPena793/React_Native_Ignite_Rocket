4c190c2f77c507ed9bcfdedcc2954bc5ee9405ad
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2023-01-09 22:29:30 -0300

    handle custom Error

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -4,11 +4,13 @@ import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { Alert } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 
 import { Container, Content, Icon } from './styles'
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
 
 export function NewGroup() {
 
@@ -16,13 +18,23 @@ export function NewGroup() {
   const navigation = useNavigation()
 
   async function handleNew() {
+ 
     try {
-      await groupCreate(group)
-      navigation.navigate('players', { group })
-    } catch (error) {
-      console.log(error)
-    }
+      if(group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", 'informe o nome da turma')
+      }
 
+      await groupCreate(group);
+      navigation.navigate('players', { group });
+
+    } catch (error) {
+      if(error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error)
+      }
+    }
   }
 
   return (
