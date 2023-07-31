6c79277b59c56092f537a71a478588fe2e17b519
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2023-07-31 20:38:21 -0300

    implemting imagePicker and FileSystem

--- ignite-gyn/src/screens/Profile.tsx
+++ ignite-gyn/src/screens/Profile.tsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { Center, Heading, ScrollView, Skeleton, VStack, useToast } from "native-base";
+import * as ImagePicker from "expo-image-picker"
+import * as FileSystem from 'expo-file-system';
+
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, VStack } from "native-base";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoLoading, setPhotoLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('http://github.com/gabrielgpena793.png')
+
+  const toast = useToast()
+
+  async function handleUserPhotoSelect() {
+
+    setPhotoLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+      })
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+
+      if(photoInfo.exists && (photoInfo.size / 1024 / 1024) > 5) {
+        return toast.show({
+          title: "Imagem muito grande, tamanho máximo suportado 5MB",
+          placement: 'top',
+          bgColor: 'red.500'
+
+        })
+      }
+
+      setUserPhoto(photoSelected.assets[0].uri)
+    } catch (error) {
+      console.log('handleUserPhotoSelect', error)
+    }
+
+    setPhotoLoading(false)
+  }
 
   return (
     <VStack flex={1}>
@@ -27,13 +67,13 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: 'http://github.com/gabrielgpena793.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Heading color='green.500' fontSize='md' mt={3} mb={8}>
               Alterar foto
             </Heading>
